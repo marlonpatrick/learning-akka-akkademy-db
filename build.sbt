e2e708d5d43aaacba1943166e27bef05d1b68385
Marlon Patrick <marlonpatrick10@gmail.com>
2018-01-07 21:40:27 -0300

    Learning Akka Chapter 1

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "akkademy-db"
+
+version := "1.0"
+
+scalaVersion := "2.12.2"
+
+lazy val akkaVersion = "2.5.8"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+  "junit" % "junit" % "4.12")
---
+++ src/test/java/com/akkademy/AkkademyDbTest.java
@@ -0,0 +1,26 @@
+package com.akkademy;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.testkit.TestActorRef;
+import com.akkademy.message.SetRequest;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AkkademyDbTest {
+
+    private ActorSystem system = ActorSystem.create();
+
+    @Test
+    public void itShouldPlaceKeyValueFromSetMessageIntoMap() {
+        TestActorRef<AkkademyDb> actorRef = TestActorRef.create(system, Props.create(AkkademyDb.class));
+
+        actorRef.tell(new SetRequest("key", "value"), ActorRef.noSender());
+
+        AkkademyDb akkademyDb = actorRef.underlyingActor();
+
+        assertEquals(akkademyDb.map.get("key"), "value");
+    }
+}
